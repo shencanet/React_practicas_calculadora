f0ff9dbf4ecc8d88eb23edb13437e5b141ba449b
shencanet <santi.dionis@gmail.com>
2022-08-04 18:02:27 +0200

    estructura contenedores css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import freecodecamplogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-  
+    <div className='freecodecamp-logo-contenedor'>
+    <img 
+      src={freecodecamplogo}
+      className='freecodecamo-logo'
+      alt= 'Logo '
+    />
+    <div className='contenedor-calculadora'>
+    <div className='fila'></div>
+    <div className='fila'></div>
+    <div className='fila'></div>
+    <div className='fila'></div>
+    <div className='fila'></div>
+
+    </div>
+
+    </div>
 
     </div>
   );
